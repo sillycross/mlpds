02aa9db94181de197eb1839979895e0cc1b50ef9
Haoran Xu <haoranxu510@gmail.com>
2018-11-24 22:36:40 -0500

    re-run all benchmarks to reflect my new dram chip

--- MlpSetUInt64Test.cpp
+++ MlpSetUInt64Test.cpp
@@ -807,7 +807,7 @@ void NO_INLINE MlpSetExecuteWorkload(WorkloadUInt64& workload)
 {
 	printf("MlpSet executing workload, enforced dependency = %d\n", (enforcedDep ? 1 : 0));
 	MlpSetUInt64::MlpSet ms;
-	ms.Init(std::min(uint64_t(67108864), workload.numInitialValues + 1000));	// TODO: remove this after I get my memory chip..
+	ms.Init(workload.numInitialValues + 1000);
 	
 	printf("MlpSet populating initial values..\n");
 	{
@@ -889,7 +889,7 @@ void NO_INLINE MlpSetExecuteWorkload(WorkloadUInt64& workload)
 void NO_INLINE MlpSetExecuteWorkloadPromise(WorkloadUInt64& workload)
 {
 	MlpSetUInt64::MlpSet ms;
-	ms.Init(std::min(uint64_t(67108864), workload.numInitialValues + 1000));	// TODO: remove this after I get my memory chip..
+	ms.Init(workload.numInitialValues + 1000);	
 	
 	printf("MlpSet populating initial values..\n");
 	{
